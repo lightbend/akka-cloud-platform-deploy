680b1cf407ce8c660b65c68294d38c42a035d2b1
Marcos Pereira <marcos.silva@gmail.com>
2021-07-02 14:30:39 -0400

    Remove eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,5 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
     ],
 };
