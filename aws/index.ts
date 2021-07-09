7c8efd1bc54b450ba6bf6e795e2519f21cdc3cce
Yury Gribkov <yury.gribkov@gmail.com>
2021-07-09 08:25:16 -0700

    Fix formatting

--- aws/index.ts
+++ aws/index.ts
@@ -123,7 +123,6 @@ export const jdbcSecret = jdbcSecretName;
 
 // AWS OTel Collector
 if (config.installAwsOTelCollector) {
-
   // Create an AWS OTel Collector namespace
   const namespaceName = config.awsOTelCollectorNamespace;
   const namespace = new k8s.core.v1.Namespace(
@@ -140,89 +139,103 @@ if (config.installAwsOTelCollector) {
   const awsOTelCollectorLabels = { app: awsOTelCollector };
 
   // AWS OTel Collector Configuration. Read from the `otel-agent-config.yaml` file.
-  const localConfig = "otel-agent-config.yaml"
-  const podConfig = "config.yaml"
-  const podMountFolder = "/etc/otel-agent-config"
-  const podMountPath = podMountFolder + "/" + podConfig
-  const awsOTelConfigMap = new k8s.core.v1.ConfigMap(awsOTelCollector, {
-    metadata: {
-      namespace: namespaceName,
-      labels: awsOTelCollectorLabels,
-      name: "aws-otel-config.yaml"
+  const localConfig = "otel-agent-config.yaml";
+  const podConfig = "config.yaml";
+  const podMountFolder = "/etc/otel-agent-config";
+  const podMountPath = podMountFolder + "/" + podConfig;
+  const awsOTelConfigMap = new k8s.core.v1.ConfigMap(
+    awsOTelCollector,
+    {
+      metadata: {
+        namespace: namespaceName,
+        labels: awsOTelCollectorLabels,
+        name: "aws-otel-config.yaml",
+      },
+      data: { [podConfig]: fs.readFileSync(localConfig).toString() },
     },
-    data: { [podConfig]: fs.readFileSync(localConfig).toString() },
-  }, { provider: cluster.k8sProvider });
-  const awsOTelConfigMapName = awsOTelConfigMap.metadata.name
+    { provider: cluster.k8sProvider },
+  );
+  const awsOTelConfigMapName = awsOTelConfigMap.metadata.name;
 
-  const configVolumeName = "config"
-  const zipkinPort = 9411
-  const healthCheckPort = 13133 // default health-check port, can be overridden in `otel-agent-config.yaml` in the health_check section
+  const configVolumeName = "config";
+  const zipkinPort = 9411;
+  const healthCheckPort = 13133; // default health-check port, can be overridden in `otel-agent-config.yaml` in the health_check section
 
   // Create an AWS OTel Collector deployment
-  const awsOTelCollectorDeployment = new k8s.apps.v1.Deployment(`${awsOTelCollector}-dep`, {
-      metadata: { 
+  const awsOTelCollectorDeployment = new k8s.apps.v1.Deployment(
+    `${awsOTelCollector}-dep`,
+    {
+      metadata: {
         namespace: namespaceName,
         labels: awsOTelCollectorLabels,
       },
       spec: {
-          minReadySeconds: 5,
-          progressDeadlineSeconds: 120,
-          replicas: 1,
-          selector: { matchLabels: awsOTelCollectorLabels },
-          template: {
-              metadata: { labels: awsOTelCollectorLabels },
-              spec: {
-                  containers: [{
-                      name: awsOTelCollector,
-                      image: "amazon/aws-otel-collector:latest",
-                      command: [ "/awscollector" ],
-                      args: [
-                        "--config=" + podMountPath, 
-                        // "--log-level=DEBUG"
-                      ],
-                      volumeMounts: [{ name: configVolumeName, mountPath: podMountFolder }],
-                      resources: {
-                        limits: {
-                          cpu:  "256m",
-                          memory: "512Mi"
-                        },
-                        requests: {
-                          cpu: "32m",
-                          memory: "24Mi"
-                        }
-                      },
-                      ports: [{ containerPort: zipkinPort }],
-                      livenessProbe: { httpGet: { path: "/", port: healthCheckPort } },
-                      readinessProbe: { httpGet: { path: "/", port: healthCheckPort } },
-                      env: [
-                        // AWS region and credentials to connect to XRay
-                        // TODO check that all credentials are set
-                        { name: "AWS_REGION", value: config.awsXRayRegion },
-                        { name: "AWS_ACCESS_KEY_ID", value: config.awsXRayAccessKeyID },
-                        { name: "AWS_SECRET_ACCESS_KEY", value: config.awsXRaySecretAccessKey }
-                      ]
-                    }],
-                  volumes: [{
-                      name: configVolumeName,
-                      configMap: { name: awsOTelConfigMapName }
-                  }],
-              }
-          }
+        minReadySeconds: 5,
+        progressDeadlineSeconds: 120,
+        replicas: 1,
+        selector: { matchLabels: awsOTelCollectorLabels },
+        template: {
+          metadata: { labels: awsOTelCollectorLabels },
+          spec: {
+            containers: [
+              {
+                name: awsOTelCollector,
+                image: "amazon/aws-otel-collector:latest",
+                command: ["/awscollector"],
+                args: [
+                  "--config=" + podMountPath,
+                  // "--log-level=DEBUG"
+                ],
+                volumeMounts: [{ name: configVolumeName, mountPath: podMountFolder }],
+                resources: {
+                  limits: {
+                    cpu: "256m",
+                    memory: "512Mi",
+                  },
+                  requests: {
+                    cpu: "32m",
+                    memory: "24Mi",
+                  },
+                },
+                ports: [{ containerPort: zipkinPort }],
+                livenessProbe: { httpGet: { path: "/", port: healthCheckPort } },
+                readinessProbe: { httpGet: { path: "/", port: healthCheckPort } },
+                env: [
+                  // AWS region and credentials to connect to XRay
+                  // TODO check that all credentials are set
+                  { name: "AWS_REGION", value: config.awsXRayRegion },
+                  { name: "AWS_ACCESS_KEY_ID", value: config.awsXRayAccessKeyID },
+                  { name: "AWS_SECRET_ACCESS_KEY", value: config.awsXRaySecretAccessKey },
+                ],
+              },
+            ],
+            volumes: [
+              {
+                name: configVolumeName,
+                configMap: { name: awsOTelConfigMapName },
+              },
+            ],
+          },
+        },
       },
-  }, { provider: cluster.k8sProvider });
-  
+    },
+    { provider: cluster.k8sProvider },
+  );
+
   // Create an AWS OTel Collector service
-  const awsOTelCollectorService = new k8s.core.v1.Service(`${awsOTelCollector}-svc`, {
-      metadata: { 
+  const awsOTelCollectorService = new k8s.core.v1.Service(
+    `${awsOTelCollector}-svc`,
+    {
+      metadata: {
         labels: awsOTelCollectorLabels,
-        namespace: namespaceName
+        namespace: namespaceName,
       },
       spec: {
-          type: ServiceSpecType.LoadBalancer,
-          ports: [ { port: zipkinPort } ],
-          selector: awsOTelCollectorLabels,
+        type: ServiceSpecType.LoadBalancer,
+        ports: [{ port: zipkinPort }],
+        selector: awsOTelCollectorLabels,
       },
-  }, { provider: cluster.k8sProvider });
-
+    },
+    { provider: cluster.k8sProvider },
+  );
 }
-
